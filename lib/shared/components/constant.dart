4c5f9f10f4a82d0809150de9ac869a098b0302c4
Yousef Sherif <yousefsherif979@gmail.com>
2026-04-19 08:48:53 +0200

    onBoarding, login and register with firebase is done

---
+++ lib/shared/components/constant.dart
@@ -0,0 +1,38 @@
+import 'package:minute_minder/shared/components/components.dart';
+import 'package:minute_minder/shared/network/local/cache_helper.dart';
+
+String ?token ;
+
+
+String ?uId ;
+
+String ?currentUserName;
+
+
+bool? darkCopy ;
+
+
+
+void signOut(context)
+{
+  CacheHelper.removeData(key: 'token').then((value)
+  {
+    if(value == true)
+    {
+      // navigateAndFinish(context, SocialLoginNewScreen(),);
+    }
+  });
+}
+
+
+
+
+
+
+
+void printFullText(String text)
+{
+  final pattern = RegExp('.{1,8000}');
+  pattern.allMatches(text).forEach((match) => print(match.group(0)));
+
+}
